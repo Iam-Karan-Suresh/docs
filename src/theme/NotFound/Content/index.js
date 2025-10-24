976b5b2a97128724d5443db9efbc51389f45b762
Iam-karan-suresh <karansuresh.info@gmail.com>
2025-10-24 16:18:03 +0530

    fix: 404 page and Redirection
    Signed-off-by: Iam-karan-suresh <karansuresh.info@gmail.com>

--- src/theme/NotFound/Content/index.js
+++ src/theme/NotFound/Content/index.js
@@ -1,31 +1,27 @@
-import React from "react";
-import {translate} from "@docusaurus/Translate";
-import Link from "@docusaurus/Link";
+import React, { useEffect } from "react";
 import {PageMetadata} from "@docusaurus/theme-common";
+import {useLocation} from '@docusaurus/router';
+import NotFoundPage from "../../../components/NotFoundPage";
 
 export default function NotFound() {
+  const location = useLocation();
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // Check if we're already in the /docs path
+      const newPath = location.pathname.startsWith('/docs') 
+        ? '/docs/' 
+        : '/docs' + location.pathname;
+      window.location.href = newPath;
+    }, 1000); // Increased to 1 seconds to match animation
+
+    return () => clearTimeout(timer);
+  }, [location]);
+
   return (
     <>
-      <PageMetadata title="Page Not Found" />
-      <main className="container margin-vert--xl">
-        <div className="row">
-          <div className="col col--6 col--offset-3 text-center">
-            <h1 className="hero__title">404: Page Not Found</h1>
-            <p className="padding-vert--md">
-              We could not find what you were looking for.<br />
-              The page you requested may have been moved or deleted.
-            </p>
-            <div className="margin-vert--lg">
-              <Link to="/docs/" className="button button--primary margin-right--md">
-                Back to Homepage
-              </Link>
-              <Link to="/docs/search" className="button button--secondary">
-                Search Documentation
-              </Link>
-            </div>
-          </div>
-        </div>
-      </main>
+      <PageMetadata title="404: Page Not Found" />
+      <NotFoundPage />
     </>
   );
 }
